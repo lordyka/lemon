40f9995ed2d6e3044c5c0b1ac70b9d72e3fbfef6
lordyka <lordyka@gmail.com>
2019-07-18 19:45:00 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -32,136 +32,136 @@ class Clock:
 
     # Numbers
     def displayZero(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        bothSides(x, y-1)
-        bothSides(x, y-2)
-        bothSides(x, y-3)
-        bothSides(x, y-4)
-        bothSides(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.bothSides(x, y-1)
+        self.bothSides(x, y-2)
+        self.bothSides(x, y-3)
+        self.bothSides(x, y-4)
+        self.bothSides(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displayOne(x, y):
-        clearNumberPixels(x, y)
-        rightSide(x, y)
-        rightSide(x, y-1)
-        rightSide(x, y-2)
-        rightSide(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        rightSide(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.rightSide(x, y)
+        self.rightSide(x, y-1)
+        self.rightSide(x, y-2)
+        self.rightSide(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.rightSide(x, y-6)
         unicorn.show()
 
     def displayTwo(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        rightSide(x, y-1)
-        rightSide(x, y-2)
-        fullLine(x, y-3)
-        leftSide(x, y-4)
-        leftSide(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.rightSide(x, y-1)
+        self.rightSide(x, y-2)
+        self.fullLine(x, y-3)
+        self.leftSide(x, y-4)
+        self.leftSide(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displayThree(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        rightSide(x, y-1)
-        rightSide(x, y-2)
-        fullLine(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.rightSide(x, y-1)
+        self.rightSide(x, y-2)
+        self.fullLine(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displayFour(x, y):
-        clearNumberPixels(x, y)
-        bothSides(x, y)
-        bothSides(x, y-1)
-        bothSides(x, y-2)
-        fullLine(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        rightSide(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.bothSides(x, y)
+        self.bothSides(x, y-1)
+        self.bothSides(x, y-2)
+        self.fullLine(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.rightSide(x, y-6)
         unicorn.show()
 
     def displayFive(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        leftSide(x, y-1)
-        leftSide(x, y-2)
-        fullLine(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.leftSide(x, y-1)
+        self.leftSide(x, y-2)
+        self.fullLine(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displaySix(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        leftSide(x, y-1)
-        leftSide(x, y-2)
-        fullLine(x, y-3)
-        bothSides(x, y-4)
-        bothSides(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.leftSide(x, y-1)
+        self.leftSide(x, y-2)
+        self.fullLine(x, y-3)
+        self.bothSides(x, y-4)
+        self.bothSides(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displaySeven(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        rightSide(x, y-1)
-        rightSide(x, y-2)
-        rightSide(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        rightSide(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.rightSide(x, y-1)
+        self.rightSide(x, y-2)
+        self.rightSide(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.rightSide(x, y-6)
         unicorn.show()
 
     def displayEight(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        bothSides(x, y-1)
-        bothSides(x, y-2)
-        fullLine(x, y-3)
-        bothSides(x, y-4)
-        bothSides(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.bothSides(x, y-1)
+        self.bothSides(x, y-2)
+        self.fullLine(x, y-3)
+        self.bothSides(x, y-4)
+        self.bothSides(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displayNine(x, y):
-        clearNumberPixels(x, y)
-        fullLine(x, y)
-        bothSides(x, y-1)
-        bothSides(x, y-2)
-        fullLine(x, y-3)
-        rightSide(x, y-4)
-        rightSide(x, y-5)
-        fullLine(x, y-6)
+        self.clearNumberPixels(x, y)
+        self.fullLine(x, y)
+        self.bothSides(x, y-1)
+        self.bothSides(x, y-2)
+        self.fullLine(x, y-3)
+        self.rightSide(x, y-4)
+        self.rightSide(x, y-5)
+        self.fullLine(x, y-6)
         unicorn.show()
 
     def displayNumber(x,y, number):
         if number == 0:
-            displayZero(x,y)
+            self.displayZero(x,y)
         elif number == 1:
-            displayOne(x,y)
+            self.displayOne(x,y)
         elif number == 2:
-            displayTwo(x,y)
+            self.displayTwo(x,y)
         elif number == 3:
-            displayThree(x,y)
+            self.displayThree(x,y)
         elif number == 4:
-            displayFour(x,y)
+            self.displayFour(x,y)
         elif number == 5:
-            displayFive(x,y)
+            self.displayFive(x,y)
         elif number == 6:
-            displaySix(x,y)
+            self.displaySix(x,y)
         elif number == 7:
-            displaySeven(x,y)
+            self.displaySeven(x,y)
         elif number == 8:
-            displayEight(x,y)
+            self.displayEight(x,y)
         elif number == 9:
-            displayNine(x,y)
+            self.displayNine(x,y)
 
     # Clears the pixels in a rectangle. x,y is the top left corner of the rectangle
     # and its dimensions are 7X7
@@ -182,41 +182,41 @@ class Clock:
         parts = datetime.datetime.now().strftime(timePart)
         return [int(x) for x in parts]  
 
-    displayedHourParts = getTimeParts('%H')
-    displayedMinuteParts = getTimeParts('%M')
+    displayedHourParts = self.getTimeParts('%H')
+    displayedMinuteParts = self.getTimeParts('%M')
 
     # Display Current Time
-    displayNumber(0,15, displayedHourParts[0])
-    displayNumber(8,15, displayedHourParts[1])
-    displayNumber(0,6, displayedMinuteParts[0])
-    displayNumber(8,6, displayedMinuteParts[1])
+    self.displayNumber(0,15, displayedHourParts[0])
+    self.displayNumber(8,15, displayedHourParts[1])
+    self.displayNumber(0,6, displayedMinuteParts[0])
+    self.displayNumber(8,6, displayedMinuteParts[1])
 
 
     try:
         while True:
-            hourParts = getTimeParts('%H')
-            minuteParts = getTimeParts('%M')
+            hourParts = self.getTimeParts('%H')
+            minuteParts = self.getTimeParts('%M')
     
             # TIME Details
             # Only update first hour number if it is different to what is currently displayed
             if hourParts[0] != displayedHourParts[0]:
                 displayedHourParts[0] = hourParts[0]
-                displayNumber(0,15, hourParts[0])
+                self.displayNumber(0,15, hourParts[0])
 
             # Only update second hour number if it is different to what is currently displayed
             if hourParts[1] != displayedHourParts[1]:
                 displayedHourParts[1] = hourParts[1]
-                displayNumber(8,15, hourParts[1])
+                self.displayNumber(8,15, hourParts[1])
 
             # Only update first minute number if it is different to what is currently displayed
             if minuteParts[0] != displayedMinuteParts[0]:
                 displayedMinuteParts[0] = minuteParts[0]
-                displayNumber(0,6, minuteParts[0])
+                self.displayNumber(0,6, minuteParts[0])
 
             # Only update second minute number if it is different to what is currently displayed
             if minuteParts[1] != displayedMinuteParts[1]:
                 displayedMinuteParts[1] = minuteParts[1]
-                displayNumber(8,6, minuteParts[1])
+                self.displayNumber(8,6, minuteParts[1])
 
             # Sleep for 0.5 because the display doesn't need to update that often
             time.sleep(1)
